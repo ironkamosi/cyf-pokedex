71cbf3e77d12ad5194427cb57f6e19e77e61feb6
ironkamosi <alintonchambers@gmail.com>
2021-03-20 15:41:30 +0100

    completed exercise d and the additional exercise

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import BestPokemon from "./BestPokemon";
 import CaughtPokemon from "./CaughtPokemon";
 
 function App() {
- function logWhenClicked() {
-   console.log("I am grateful");
- }  
+  function logWhenClicked() {
+    console.log("I am grateful");
+  }
   return (
     <div>
       <Logo appName="Pokedex" handleClick={logWhenClicked} />
-      <BestPokemon abilities={["Anticipation", "Adaptability", "Run-Away"]}/>
-      <CaughtPokemon date={new Date().toLocaleDateString()}
-/>
+      <BestPokemon abilities={["Anticipation", "Adaptability", "Run-Away"]} />
+      <CaughtPokemon date={new Date().toLocaleDateString()} />
     </div>
-  )
+  );
 }
 export default App;
--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React,{useState} from "react"
 
 const CaughtPokemon = (props) => {
     console.log(props)
+    const [caught, setCaught] = useState(0);
+    const CatchPokemon = () => {
+    //    setCaught(caught + 1)
+        setCaught(Math.floor(Math.random(100)*100))
+    };
     // let date = new Date().toLocaleDateString();
     return (
         <div>
             <p>
-                Caught 0 Pokemon on {props.date}
+                Caught {caught} Pokemon on {props.date}
             </p>
+            <button onClick={CatchPokemon}><h3>Catch a Pokemon</h3></button>
         </div>
     )
 }
