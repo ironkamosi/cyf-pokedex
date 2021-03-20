62e5bbb1a60b06ea2549e0f72ea29144ed744b16
ironkamosi <alintonchambers@gmail.com>
2021-03-20 16:22:20 +0100

    completed exercise E and began the extension activity

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,21 +1,39 @@
-import React,{useState} from "react"
+import React, { useState } from "react";
 
 const CaughtPokemon = (props) => {
-    console.log(props)
-    const [caught, setCaught] = useState(0);
-    const CatchPokemon = () => {
-    //    setCaught(caught + 1)
-        setCaught(Math.floor(Math.random(100)*100))
-    };
-    // let date = new Date().toLocaleDateString();
-    return (
-        <div>
-            <p>
-                Caught {caught} Pokemon on {props.date}
-            </p>
-            <button onClick={CatchPokemon}><h3>Catch a Pokemon</h3></button>
-        </div>
-    )
-}
+  //   console.log(props)
+
+//   const pokemonCaught = [
+//     "Rayquaza",
+//     "Garchomp",
+//     "Sylveon",
+//     "Umbreon",
+//     "Charizard",
+//     "Mimikyu",
+//     "Lucario",
+//     "Greninja",
+//   ];// pokemonCaught[Math.random(pokemonCaught.length)]
+  const [caught, setCaught] = useState([]);
+  const CatchPokemon = () => {
+    setCaught(caught.concat("Garchomp"));
+    // setCaught(Math.floor(Math.random(100)*100))
+  };
+  // let date = new Date().toLocaleDateString();
+  return (
+    <div>
+      <p>
+        Caught {caught.length} Pokemon on {props.date}
+      </p>
+      <ul>
+        {caught.map((pokemon, index) => {
+          return <li key={index}>{pokemon}</li>;
+        })}
+      </ul>
+      <button onClick={CatchPokemon}>
+        <h3>Catch a Pokemon</h3>
+      </button>
+    </div>
+  );
+};
 
 export default CaughtPokemon;
