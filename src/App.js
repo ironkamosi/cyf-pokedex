79d19f3e7e230c13d334e1a8efc801ee853e6aa2
ironkamosi <alintonchambers@gmail.com>
2021-03-27 15:13:16 +0100

    week 3 exercise a-e

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Logo from "./Logo";
 import BestPokemon from "./BestPokemon";
 import CaughtPokemon from "./CaughtPokemon";
+import PokemonMovesSelector from "./PokemonMovesSelector";
 
 function App() {
   function logWhenClicked() {
@@ -12,7 +13,34 @@ function App() {
       <Logo appName="Pokedex" handleClick={logWhenClicked} />
       <BestPokemon abilities={["Anticipation", "Adaptability", "Run-Away"]} />
       <CaughtPokemon date={new Date().toLocaleDateString()} />
+      <PokemonMovesSelector/>
+    
     </div>
   );
 }
 export default App;
+
+
+/****
+ function MartianPhotoFetcher() {
+  const [marsPhotoData, setMarsPhotoData] = useState(null);
+
+  useEffect(() => {
+    console.log("Fetching data from NASA");
+
+    fetch(
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setMarsPhotoData(data);
+      });
+  }, []);
+
+  if (marsPhotoData) {
+    return <img src={marsPhotoData.photos[0].img_src} alt="Martian surface" />;
+  } else {
+    return null;
+  }
+}
+ */
